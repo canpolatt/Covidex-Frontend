9bb54f9389784946f1580f358078bf8d9f6b6f5a
Can POLAT <can.polatt@yahoo.com>
2021-06-14 20:19:14 +0300

    🌱 14.06.2021 Dashboard tasarımları yapıldı (CSS dosyaları refactor edilecek)

--- src/pages/DashboardMainPage.js
+++ src/pages/DashboardMainPage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import "../DashboardMainPage.scss"
+import { PieChart, Pie, Cell } from 'recharts';
+import {ReactComponent as MaskIcon} from "../svgs/masks_black_24dp.svg";
+import HumanVector from "../images/output-onlinepngtools.png"
 
 function DashboardMainPage() {
+    const data = [
+        { name: 'Group A', value: 600 },
+        { name: 'Group B', value: 150 },
+        { name: 'Group C', value: 250 }
+    ];
+    const COLORS = ['#FF8E30', '#EA3221', '#4BC337'];
     return (
         <div className="dashboardMainPage">
-           <div className="cardsWrap">
+            <div className="row1">
+                <div className="cardsWrap">
                <h2>COVID-19 - Report</h2>
                <div className="cardsWrapInner">
                    <div className="card">
@@ -33,14 +43,54 @@ function DashboardMainPage() {
                    </div>
                </div>
            </div>
-            <div className="card2">
-                asddsadi
-                şqwe
-                şewqşi
-                wqeşi
-                ewqşi
-                qewşieqw
             </div>
+            <div className="row2">
+                 <div className="card2">
+                     <p>Total cases : 124,916</p>
+                <PieChart width={800} height={400}>
+                    <Pie
+                        data={data}
+                        innerRadius={60}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        paddingAngle={5}
+                        dataKey="value"
+                    >
+
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+                     <div className="card2-sub">
+                         <ul>
+                            <span>🟢</span> <li>Recovered 50%</li>
+                            <span>🔴</span> <li>Deaths 30%</li>
+                            <span>🟠</span> <li>Active 20%</li>
+                         </ul>
+                     </div>
+            </div>
+                <div className="card3">
+                   <div className="row3">
+                       <div className="card4">
+                           <div><MaskIcon/></div>
+                          <p>Show that you care.
+                          Wear.</p>
+                       </div>
+                   </div>
+                    <div className="row3">
+                        <div className="card5">
+                            <p>Feeling a little under the weather ?
+                            <span> Take Test</span>
+                            </p>
+                            <img src={HumanVector} alt="image"/>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     );
 }
---
+++ src/services/CovidStatsService.js
@@ -0,0 +1,7 @@
+import axios from "axios"
+
+export default  class covidStatsService {
+    getCovidstats(){
+        return axios.get()
+    }
+}
